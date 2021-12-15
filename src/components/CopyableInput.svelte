059ad09a63e649d751b447308ff5afa228b959be
Justin Golden <justingolden21@gmail.com>
2021-12-15 01:28:07 -0800

    copy btns working

--- src/components/CopyableInput.svelte
+++ src/components/CopyableInput.svelte
@@ -3,11 +3,34 @@
 
 	export let value;
 	export let onChange;
+
+	let input;
 </script>
 
 <div class="relative {$$props.class ?? ''}">
-	<input class="surface w-full h-8" type="text" {value} on:change={onChange} />
+	<input
+		bind:this={input}
+		spellcheck="false"
+		class="surface w-full h-8"
+		type="text"
+		{value}
+		on:change={onChange}
+	/>
 	<button
+		on:click={(evt) => {
+			const str = input.value;
+			input.focus();
+			input.setSelectionRange(0, str.length);
+
+			navigator.clipboard.writeText(str).then(
+				() => {
+					console.log('clipboard successfully set');
+				},
+				() => {
+					console.log('clipboard write failed');
+				}
+			);
+		}}
 		class="surface absolute inset-y-0 right-0 flex items-center hover:bg-gray-100 focus:bg-gray-100"
 	>
 		<Icon name="copy" class="w-6 h-6" />
