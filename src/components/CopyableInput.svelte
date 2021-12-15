480c82ab96ca703b79baea606d653229ee801763
Justin Golden <justingolden21@gmail.com>
2021-12-15 00:10:52 -0800

    extrapolate copyable input code

---
+++ src/components/CopyableInput.svelte
@@ -0,0 +1,15 @@
+<script>
+	import Icon from './Icon.svelte';
+
+	export let value;
+	export let onChange;
+</script>
+
+<div class="relative">
+	<input class="surface w-full h-8" type="text" {value} on:change={onChange} />
+	<button
+		class="surface absolute inset-y-0 right-0 flex items-center hover:bg-gray-100 focus:bg-gray-100"
+	>
+		<Icon name="copy" class="w-6 h-6" />
+	</button>
+</div>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -3,6 +3,7 @@
 	import { onMount } from 'svelte';
 	import 'dino-color-picker';
 	import Icon from '../components/Icon.svelte';
+	import CopyableInput from '../components/CopyableInput.svelte';
 
 	import w3color from '../js/lib/w3color';
 
@@ -61,6 +62,7 @@
 		class="surface"
 		placeholder={$session.languageDictionary.messages['Type a color...']}
 		on:change={readColorString}
+		on:focus={(evt) => evt.target.select()}
 	/>
 	<button
 		class="w-16 h-16 rounded"
@@ -85,22 +87,16 @@
 	<div class="surface">
 		<h3>RGB</h3>
 
-		<div class="relative">
-			<input class="surface w-full h-8" type="text" value={rgbString} />
-			<button
-				class="surface absolute inset-y-0 right-0 flex items-center hover:bg-gray-100 focus:bg-gray-100"
-			>
-				<Icon name="copy" class="w-6 h-6" />
-			</button>
-		</div>
+		<CopyableInput value={rgbString} onChange={readColorString} />
 
 		<input class="surface w-full h-8" type="text" on:change={readColorString} value={hexString} />
+		<input class="surface w-full h-8" type="text" on:change={setRedString} value={red} />
+	</div>
+	<div class="surface">
 		<input class="surface w-full h-8" type="text" on:change={readColorString} value={hslString} />
 		<input class="surface w-full h-8" type="text" on:change={readColorString} value={hwbString} />
 		<input class="surface w-full h-8" type="text" on:change={readColorString} value={cmykString} />
-		<input class="surface w-full h-8" type="text" on:change={setRedString} value={red} />
 	</div>
-	<div class="surface">hi</div>
 	<div class="surface">hi</div>
 	<div class="surface">hi</div>
 </div>
