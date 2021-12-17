ef97195465b8c09bd87745f855f9cb6e6b8d9bdc
Justin Golden <justingolden21@gmail.com>
2021-12-16 22:48:25 -0800

    add comment on bad ux

--- src/components/Range.svelte
+++ src/components/Range.svelte
@@ -19,6 +19,8 @@
 		@apply appearance-none w-full h-2 rounded-full borders opacity-75 transition-opacity hover:opacity-100;
 	}
 
+	/* bad UX to possibly make slider thumb white? unable to add border to thumb... */
+
 	.slider::-webkit-slider-thumb {
 		@apply appearance-none w-5 h-5 rounded-full cursor-pointer;
 		background-color: var(--current-color);
