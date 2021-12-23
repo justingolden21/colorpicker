30645306723f967ac636cd121a6191ccaadbfccf
Justin Golden <justingolden21@gmail.com>
2021-12-23 12:24:01 -0800

    update colorlist look and feel

--- src/components/ColorList.svelte
+++ src/components/ColorList.svelte
@@ -4,13 +4,21 @@
 	// will use this componen t twice, one for history and one for favs
 	// basically an array of items with crud opperations, plus rendering and editing and opening colors
 
+	// future: add sorting by date and display date options
+
+	import Icon from './Icon.svelte';
+
 	export let items = [];
 
-	// export const setItems = () => {};
+	// export const set = (newItems) => {
+	// 	items = newItems;
+	// };
 
 	const remove = (timestamp) => {
 		items = items.filter((item) => item.timestamp != timestamp);
 	};
+
+	// todo: dont add duplicate items, check if prev item matches current
 	export const add = (item) => {
 		items = items.concat({
 			...item,
@@ -20,19 +28,30 @@
 	const removeAll = () => (items = []);
 </script>
 
-{#if items.length > 0}
+{#if items && items.length > 0}
 	<p>{items.length} items</p>
+	<button on:click={removeAll}>remove all</button>
 	<div>
 		{#each items as item, idx (item.timestamp)}
-			<div class="w-4 h-4 rounded" style="background-color: {item.color}" />
-			<p>{item.color}</p>
-			{#if item.name}
-				<p>{item.name}</p>
-			{/if}
-			<p>{item.timestamp}</p>
-			<button on:click={() => remove(item.timestamp)}>x</button>
+			<div class="surface grid grid-cols-2 md:grid-cols-4 gap-2">
+				<div>
+					<div class="w-8 h-8 rounded" style="background-color: {item.color}" />
+					<span>{item.color}</span>
+				</div>
+				<div>
+					<p>{item.name}</p>
+				</div>
+				<div>
+					<button>open</button>
+					<button>edit</button>
+					<button>move</button>
+					<button class="btn">
+						<Icon name="heart" class="inline w-4 h-4" />
+					</button>
+				</div>
+				<button on:click={() => remove(item.timestamp)}>x</button>
+			</div>
 		{/each}
-		<button on:click={removeAll}>remove all</button>
 	</div>
 {/if}
 
