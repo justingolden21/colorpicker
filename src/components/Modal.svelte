76d7144da24b9a50f4aaa89618d2ddaad6c47951
Justin Golden <justingolden21@gmail.com>
2021-12-23 10:45:03 -0800

    clean modal css

--- src/components/Modal.svelte
+++ src/components/Modal.svelte
@@ -22,55 +22,36 @@
 	}}
 />
 
-<div class="modal {shown ? 'block' : 'hidden'}" on:click={() => hide()}>
-	<div class="modal-content" on:click|stopPropagation>
-		<div class="modal-header">
+<div
+	class="xs:p-3 sm:p-6 md:p-12 fixed left-0 top-0 w-full h-full bg-gray-300 bg-opacity-75 z-20 {shown
+		? 'block'
+		: 'hidden'}"
+	on:click={() => hide()}
+>
+	<div
+		class="modal-content flex flex-col h-full border-2 border-gray-300 bg-gray-100 p-2 xs:p-8 xs:pt-4 relative m-auto w-full md:w-3/4 bg-opacity-75"
+		on:click|stopPropagation
+	>
+		<div class="mb-4 border-b-2 border-gray-300 pb-9">
 			<button
-				class="close icon-btn"
+				class="icon-btn z-30 float-right mt-2"
 				on:click={() => hide()}
 				aria-label={$session.languageDictionary.labels['Close']}
 			>
 				<Icon name="close" class="w-6 h-6" />
 			</button>
-			<h1><Icon name={icon} class="w-6 h-6 inline mr-2" /> {title}</h1>
+			<h1 class="text-left mt-4"><Icon name={icon} class="w-6 h-6 inline mr-2" /> {title}</h1>
 		</div>
-		<div class="modal-body">
+		<div class="text-left relative flex-1 overflow-auto">
 			<slot />
 		</div>
 	</div>
 </div>
 
 <style lang="postcss">
-	.modal {
-		@apply xs:p-3 sm:p-6 md:p-12 fixed left-0 top-0 w-full h-full bg-gray-300 bg-opacity-75 z-20;
-	}
 	.modal-content {
-		@apply flex flex-col h-full border-2 border-gray-300 bg-gray-100 p-2 xs:p-8 xs:pt-4 relative m-auto w-full md:w-3/4 bg-opacity-75;
-		backdrop-filter: blur(4px);
 		animation: animateTop 0.25s;
 		-webkit-animation: animateTop 0.25s;
-	}
-	.modal button.close {
-		@apply z-30 float-right mt-2;
-	}
-	.modal h1 {
-		@apply text-left mt-4;
-	}
-	.modal-header {
-		@apply mb-4 border-b-2 border-gray-300 pb-9;
-	}
-	.modal-body {
-		@apply text-left relative flex-1 overflow-auto;
-	}
-
-	:global(.dark) .modal {
-		@apply bg-gray-700 bg-opacity-75;
-	}
-	:global(.dark) .modal-content {
-		@apply border-gray-200 bg-gray-800 bg-opacity-75;
-	}
-	:global(.dark) .modal-header {
-		@apply border-gray-200;
 	}
 
 	@keyframes animateTop {
